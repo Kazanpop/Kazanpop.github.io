5679c1ee7d798e41d7ffcff928ce4a64002c0ad4
Kazanpop <36130408+Kazanpop@users.noreply.github.com>
2024-03-10 10:29:41 +0300

    Add files via upload

---
+++ backup/js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+    
+    $('.load-more').on('click', function(){
+        const btn = $(this);
+        const loader = btn.find('span');
+        
+        $.ajax({
+            url: '../data.html',
+            type: 'GET',
+            beforeSend: function(){
+                btn.attr('disabled', true);
+                loader.addClass('d-inline-block');
+            },
+            success: function(responce){
+                btn.attr('disabled', false);
+                loader.removeClass('d-inline-block');
+                console.log(responce);
+            },
+            error: function(){
+                alert('Error');
+                btn.attr('disabled', false);
+                loader.removeClass('d-inline-block');
+            }
+            
+        });
+        
+    });
+    
+});
